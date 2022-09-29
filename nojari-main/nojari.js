363962ecedb5622320e4797f69c9af08f0a7e696
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2022-09-29 19:11:19 +0900

    v.6

--- nojari-main/nojari.js
+++ nojari-main/nojari.js
@@ -52,9 +52,31 @@ $("#goin").click(function () {
                     if (qw > 19) {
                         clearInterval(myVar);
                     }
-                }, 1000);
+                }, 500);
             }
         }
     })
 });
 
+$(function () {
+    $("#shot").on("click", function () {
+        // 캡쳐 라이브러리를 통해서 canvas 오브젝트를 받고 이미지 파일로 리턴한다.
+        html2canvas(document.querySelector("#div")).then(canvas => {
+            saveAs(canvas.toDataURL('image/png'), "capture-test.png");
+        });
+    });
+
+    function saveAs(uri, filename) {
+        // 캡쳐된 파일을 이미지 파일로 내보낸다.
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
+    }
+});
