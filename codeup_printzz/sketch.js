75e44da322d8dcc3c1c3b0cf8afede9a3836a059
jinsoo <89149734+Jinsoo1004@users.noreply.github.com>
2022-10-05 19:11:32 +0900

    v11

---
+++ codeup_printzz/sketch.js
@@ -0,0 +1,41 @@
+
+let font;
+let vehicles = [];
+
+function preload() {
+  font = loadFont('AvenirNextLTPro-Demi.otf');
+}
+
+function setup() {
+  createCanvas(700, 300);
+  background(51);
+  // textFont(font);
+  // textSize(192);
+  // fill(255);
+  // noStroke();
+  // text('train', 100, 200);
+
+  var points = font.textToPoints('nojari', 100, 200, 192, {
+    // sampleFactor: 0.25
+    sampleFactor: 0.15
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+    // stroke(255);
+    // strokeWeight(8);
+    // point(pt.x, pt.y);
+  }
+}
+
+function draw() {
+  background(51);
+  for (var i = 0; i < vehicles.length; i++) {
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.show();
+  }
+}
